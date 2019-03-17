531139d3ad5f6b0f9c759d765b1956990e47e9a8
Akseli Palen <akseli.palen@gmail.com>
2019-03-18 00:23:39 +0200

    Remove ;

--- src/index.js
+++ src/index.js
@@ -24,10 +24,10 @@ function playSound (context, arr) {
   for (var i = 0; i < arr.length; i++) buf[i] = arr[i]
   var buffer = context.createBuffer(1, buf.length, context.sampleRate)
   buffer.copyToChannel(buf, 0)
-  var source = context.createBufferSource();
-  source.buffer = buffer;
-  source.connect(context.destination);
-  source.start(0);
+  var source = context.createBufferSource()
+  source.buffer = buffer
+  source.connect(context.destination)
+  source.start(0)
 }
 
 function sineWave (sampleRate, sampleNumber, tone) {
