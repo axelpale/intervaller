28169f0c21c72ad530824b8f443716dc484ba791
Akseli Palen <akseli.palen@gmail.com>
2019-03-17 23:11:45 +0200

    Fix indentation

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 function playSound (context, arr) {
-    var buf = new Float32Array(arr.length)
-    for (var i = 0; i < arr.length; i++) buf[i] = arr[i]
-    var buffer = context.createBuffer(1, buf.length, context.sampleRate)
-    buffer.copyToChannel(buf, 0)
-    var source = context.createBufferSource();
-    source.buffer = buffer;
-    source.connect(context.destination);
-    source.start(0);
+  var buf = new Float32Array(arr.length)
+  for (var i = 0; i < arr.length; i++) buf[i] = arr[i]
+  var buffer = context.createBuffer(1, buf.length, context.sampleRate)
+  buffer.copyToChannel(buf, 0)
+  var source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(context.destination);
+  source.start(0);
 }
 
 function sineWave (sampleRate, sampleNumber, tone) {
